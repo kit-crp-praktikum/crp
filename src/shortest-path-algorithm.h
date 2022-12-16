b691f7d3131e8a7efbfdab5c324c466a781b9b8e
Ilia Bozhinov <ammen99@gmail.com>
2022-12-16 22:48:33 +0100

    add interface for the whole algorithm

---
+++ src/shortest-path-algorithm.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+#include "data-types.h"
+#include "graph.h"
+
+namespace crp
+{
+class CRPAlgorithm
+{
+  public:
+    virtual ~CRPAlgorithm() = default;
+
+    /**
+     * Phase 1: Initialize the routing algorithm for the given graph.
+     * Note that the graph's weights do not have to have been initialized at this point yet,
+     * and can all be equal zero.
+     */
+    virtual void prepare(Graph *graph) = 0;
+
+    /**
+     * Phase 2: Signal to the algorithm that the graph's weights have changed and it should update its
+     * internal state to reflect these changes.
+     */
+    virtual void customize() = 0;
+
+    /**
+     * Query: Compute the shortest distance between start and end.
+     */
+    virtual Distance query(NodeId start, NodeId end) = 0;
+
+    /**
+     * Query: Find the shortest path between start and end.
+     */
+    virtual std::vector<NodeId> query_path(NodeId start, NodeId end, Distance& out_dist) = 0;
+};
+
+};
