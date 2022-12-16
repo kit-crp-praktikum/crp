449b74f0ab0848c5d01bfe0178ead286cb019acb
Manuel Haag <manuel_haag@t-online.de>
2022-12-16 18:04:58 +0100

    add implementation of dinics flow algorithm

---
+++ src/algorithms/dinics.hpp
@@ -0,0 +1,167 @@
+#pragma once
+
+#include <vector>
+#include <queue>
+#include <limits>
+#include <iostream>
+#include "data-types.h"
+
+/**
+ * Implementation of dinics-flow algorithm for undirected graphs.
+*/
+class DinicsFlow {
+    using EdgeId = uint32_t;
+    using Flow = int32_t;
+
+    struct Edge {
+        NodeId to;
+        EdgeId reverse_edge; //position in graph[to]
+        Flow flow, capacity;
+        Edge(NodeId _to, EdgeId _rev, Flow _cap) : to(_to), reverse_edge(_rev), flow(0), capacity(_cap) {}
+        Flow remaining_capacity() {return capacity - flow;}
+        bool has_capacity() {return remaining_capacity() > 0;}
+    };
+
+    public:
+        DinicsFlow(uint32_t num_nodes) : 
+            graph(num_nodes + 2), //2 extra nodes for multi src and target nodes
+            work(num_nodes + 2),
+            bfs_distance(num_nodes + 2),
+            multi_src(num_nodes),
+            multi_target(num_nodes + 1),
+            num_nodes(num_nodes),
+            INF_FLOW(std::numeric_limits<Flow>::max()) {}
+
+        void add_edge(NodeId from, NodeId to, Flow capacity) {
+            graph[from].emplace_back(to, graph[to].size(), capacity);
+            graph[to].emplace_back(from, graph[from].size() - 1, capacity);
+        }
+
+        Flow max_flow(NodeId src, NodeId target) {
+            reset();
+            Flow total_flow = 0;
+            Flow new_flow = 0;
+            while(bfs(src, target)) {
+                std::fill(work.begin(), work.end(), 0);
+                do {
+                    new_flow = dfs(src, target, INF_FLOW);
+                    total_flow += new_flow;
+                } while(new_flow > 0);
+            }
+            return total_flow;
+        }
+
+        Flow multi_src_target_max_flow(std::vector<NodeId> &src_nodes, std::vector<NodeId> &target_nodes) {
+            setup_multi_src_target(src_nodes, target_nodes);
+            Flow flow =  max_flow(multi_src, multi_target);
+            reset_multi_src_target();
+            return flow;
+        }
+
+        //computes left most and right most cut, returns more balanced cut
+        std::vector<bool> min_cut_partition(NodeId src, NodeId target) {
+            max_flow(src, target);
+            auto is_residual = [](Edge &e) {return std::abs(e.flow) + 1 <= e.capacity;};
+            std::vector<bool> left_most(num_nodes);
+            std::vector<bool> right_most(num_nodes);
+
+            generic_bfs(src, is_residual);
+            for(NodeId v = 0; v < num_nodes; v++) {
+                left_most[v] = bfs_distance[v] != -1;
+            }
+            
+            generic_bfs(target, is_residual);
+            for(NodeId v = 0; v < num_nodes; v++) {
+                right_most[v] = bfs_distance[v] != -1;
+            }
+            NodeId left = std::count(left_most.begin(), left_most.end(), true);
+            NodeId right = std::count(right_most.begin(), right_most.end(), true);
+
+            if(std::min(left, num_nodes - left) >= std::min(right, num_nodes - right)) {
+                return left_most;
+            }
+            else {
+                return right_most;
+            }
+        }
+
+        std::vector<bool> multi_src_target_min_cut_partition(std::vector<NodeId> &src_nodes, std::vector<NodeId> &target_nodes) {
+            setup_multi_src_target(src_nodes, target_nodes);
+            std::vector<bool> partition = min_cut_partition(multi_src, multi_target);
+            reset_multi_src_target();
+            return partition;
+        }
+
+    private:
+        void setup_multi_src_target(std::vector<NodeId> &src_nodes, std::vector<NodeId> &target_nodes) {
+            for(NodeId v : src_nodes) {
+                add_edge(multi_src, v, INF_FLOW);
+            }
+            for(NodeId v : target_nodes) {
+                add_edge(v, multi_target, INF_FLOW);
+            }
+        }
+
+        void reset_multi_src_target() {
+            graph[multi_src].clear();
+            graph[multi_target].clear();
+        }
+
+        void reset() {
+            for(auto &v : graph) {
+                for(Edge &e : v) {
+                    e.flow = 0;
+                }
+            }
+        } 
+
+        void generic_bfs(NodeId start, auto predicate) {
+            std::fill(bfs_distance.begin(), bfs_distance.end(), -1);
+            queue.push(start);
+            bfs_distance[start] = 0;
+            while(!queue.empty()) {
+                NodeId v = queue.front(); queue.pop();
+                for(Edge &e : graph[v]) {
+                    if(bfs_distance[e.to] == -1 && predicate(e)) {
+                        bfs_distance[e.to] = 1 + bfs_distance[v];
+                        queue.push(e.to);
+                    }
+                }
+            }
+        }
+
+        //returns wether target is reachable
+        bool bfs(NodeId src, NodeId target) {
+            generic_bfs(src, [](Edge &e) {return e.has_capacity();});
+            return bfs_distance[target] != -1;
+        }
+
+        void push_flow(Edge &e, Flow flow) {
+            e.flow += flow;
+            graph[e.to][e.reverse_edge].flow -= flow;
+        }
+
+        //returns wether pushed flow
+        Flow dfs(NodeId v, NodeId target, Flow residual_flow) {
+            if(v == target || residual_flow == 0) return residual_flow;
+            for(; work[v] < graph[v].size(); work[v]++) {
+                Edge &e = graph[v][work[v]];
+                if(bfs_distance[e.to] == 1 + bfs_distance[v] && e.has_capacity()) {
+                    Flow flow = dfs(e.to, target, std::min(residual_flow, e.remaining_capacity()));
+                    if(flow > 0) {
+                        push_flow(e, flow);
+                        return flow;
+                    }
+                }
+            }
+            return 0;
+        }
+        std::vector<std::vector<Edge>> graph; 
+        std::vector<NodeId> work; //for every node, what is the next neighbor to process 
+        std::vector<int32_t> bfs_distance;
+        std::queue<NodeId> queue;
+        NodeId multi_src;
+        NodeId multi_target;
+        NodeId num_nodes;
+        Flow INF_FLOW;
+};
--- src/main_sp.cpp
+++ src/main_sp.cpp
@@ -6,6 +6,7 @@
 #include "algorithms/dijkstra.hpp"
 #include "algorithms/bellman_ford.hpp"
 #include "algorithms/floyd_warshall.hpp"
+#include "algorithms/dinics.hpp"
 #include "data-types.h"
 
 void test_timestampled_v() {
@@ -70,11 +71,59 @@ void test_sp() {
     }
 }
 
+void test_flow1() {
+    int n = 6;
+    DinicsFlow dinics(n);
+    dinics.add_edge(0, 1, 7);
+    dinics.add_edge(0, 4, 4);
+    dinics.add_edge(1, 2, 5);
+    dinics.add_edge(1, 3, 3);
+    dinics.add_edge(2, 5, 8);
+    dinics.add_edge(3, 2, 3);
+    dinics.add_edge(3, 5, 5);
+    dinics.add_edge(4, 1, 3);
+    dinics.add_edge(4, 3, 2);
+    std::cout << "\ndinics" << "\n";
+    std::cout << dinics.max_flow(0, n - 1) << "\n";
+    auto v = dinics.min_cut_partition(0, n - 1);
+    for(auto b : v) {
+        std::cout << b << " ";
+    }
+    std::cout << "\n";  
+}
+
+void test_flow2() {
+    int n = 8;
+    DinicsFlow dinics(n);
+    // C_3 - 3 - 4 - C_3
+    dinics.add_edge(0, 1, 1);
+    dinics.add_edge(0, 2, 1);
+    dinics.add_edge(1, 2, 1);
+    dinics.add_edge(2, 3, 1);
+    dinics.add_edge(3, 4, 1);
+    dinics.add_edge(4, 5, 1);
+    dinics.add_edge(5, 6, 1);
+    dinics.add_edge(5, 7, 1);
+    dinics.add_edge(6, 7, 1);
+    std::vector<NodeId> src = {0, 1, 2};
+    std::vector<NodeId> target = {5, 6, 7};
+    std::cout << "\nmultistart dinics" << "\n";
+    std::cout << dinics.multi_src_target_max_flow(src, target) << "\n";
+    auto v = dinics.multi_src_target_min_cut_partition(src, target);
+    for(auto b : v) {
+        std::cout << b << " ";
+    }
+    std::cout << "\n";  
+
+}
+
 
 int main() {
     std::cout << "hi \n";
 
     test_timestampled_v();
     test_sp();
+    test_flow1();
+    test_flow2();
 
 }
