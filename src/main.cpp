9a16c47554b14296b7dfe244c6505260f5081d45
Manuel Haag <manuel_haag@t-online.de>
2023-02-12 23:10:29 +0100

    add cmd-parameter to reorder nodes

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,7 @@ Argument description:
                      dijkstra, bf, dijkstra-rebuild, bf-rebuild, fw-rebuild, bf-simd-rebuild
 
 -t, --threads The number of threads to use.
+-r, --reorder-nodes reorder border nodes to achieve better customization and query time
 
 -h, --help Show a help message like this one.
 --dump-partition Only generate the partition of the graph and dump the data on stdout.
@@ -132,6 +133,7 @@ struct CmdLineParams
     std::string weight_type;
     OperationMode mode = OperationMode::Benchmark;
     bool unpack_paths = false;
+    bool reorder_nodes = false;
 };
 
 static partitioner::KaHIPMode parse_kahip_mode(std::string value)
@@ -406,7 +408,11 @@ CmdLineParams load_parameters_from_cmdline(int argc, char **argv)
     pos = find_required_argument(argc, argv, 'w', "weight", true);
     params.weight_type = argv[pos + 1];
     check_input_directory_valid(params.data_dir, params.weight_type, true);
-    
+    pos = find_argument_index(argc, argv, 'r', "reorder-nodes");
+    if (pos != -1)
+    {
+        params.reorder_nodes = true;
+    }
     std::string path = params.data_dir;
     if(path.back() == '/') path = path.substr(0, path.size() - 1);
     std::string graph_name = path.substr(path.find_last_of("/\\") + 1);
@@ -416,6 +422,7 @@ CmdLineParams load_parameters_from_cmdline(int argc, char **argv)
     std::cerr << "cells_per_level=" << params.algo_params.cells_per_level << "\n";
     std::cerr << "levels=" << params.algo_params.number_of_levels << "\n";
     std::cerr << "threads=" << number_of_threads << "\n";
+    std::cerr << "reorder_nodes=" << params.reorder_nodes << "\n";
 
     select_partitioner(argc, argv, params);
 
@@ -484,7 +491,8 @@ int main(int argc, char **argv)
         if (params.mode == OperationMode::CustomizeOnly)
         {
             std::vector<NodeId> map, revmap;
-            get_time_debug("node reordering", [&] { crp::CRPAlgorithm::reoder_nodes(g, rp, map, revmap); });
+            if(params.reorder_nodes)
+                get_time_debug("node reordering", [&] { crp::CRPAlgorithm::reoder_nodes(g, rp, map, revmap); });
             crp::OverlayStructure *os;
 
             auto time = get_time([&] {
